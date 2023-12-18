48da0e7879624470747c67ef501ca3389c6f91b4
jhjang7408 <jhjang7408@naver.com>
2023-12-18 17:04:23 +0900

    12월 18일 JWT 무언가 매우 잘못되었다

--- src/main/java/com/temisone/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/com/temisone/jwt/repository/RefreshTokenRepository.java
@@ -1,4 +1,10 @@
 package com.temisone.jwt.repository;
 
-public class RefreshTokenRepository {
+import com.temisone.jwt.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
 }
